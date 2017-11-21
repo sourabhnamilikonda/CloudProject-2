af0c74d7af74f18e36fa2ade4cbd3687fe7352eb
hrishikeshr <31361769+hrishikeshr@users.noreply.github.com>
2017-11-20 22:58:26 -0800

    Add files via upload

---
+++ java/com/setget/set.java
@@ -0,0 +1,61 @@
+package com.setget;
+
+
+public class set 
+{
+    
+    private String file_nm;
+    private String fileDescription;
+    private String fileUploadTime;
+    private String first_nm;
+    private String last_nm;
+    private String updateTime;
+  
+    public String getFileName() {
+        return file_nm;
+    }
+
+    public void setFileName(String file_nm) {
+        this.file_nm = file_nm;
+    }
+
+    public String getFileDescription() {
+        return fileDescription;
+    }
+
+    public void setFileDescription(String fileDescription) {
+        this.fileDescription = fileDescription;
+    }
+
+    public String getFileUploadTime() {
+        return fileUploadTime;
+    }
+
+    public void setFileUploadTime(String fileUploadTime) {
+        this.fileUploadTime = fileUploadTime;
+    }
+
+    public String getFirstName() {
+        return first_nm;
+    }
+
+    public void setFirstName(String first_nm) {
+        this.first_nm = first_nm;
+    }
+
+    public String getLastName() {
+        return last_nm;
+    }
+
+    public void setLastName(String last_nm) {
+        this.last_nm = last_nm;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }    
+}
